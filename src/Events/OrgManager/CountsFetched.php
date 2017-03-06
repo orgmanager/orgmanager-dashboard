cc4c547c1d4774550b9e74af503dcc7613e3ef9d
Miguel Piedrafita <git@miguelpiedrafita.com>
2017-03-06 21:28:10 +0100

    Add CountsFetched event

---
+++ src/Events/OrgManager/CountsFetched.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace OrgManager\OrgmanagerDashboard\Events;
+
+use App\Events\DashboardEvent;
+
+class CountsFetched extends DashboardEvent
+{
+    public $users;
+
+    public $orgs;
+
+    public $invites;
+
+    public function __construct(array $counts)
+    {
+        foreach ($counts as $countName => $value) {
+            $this->$countName = $value;
+        }
+    }
+}
