161d75d2a7d74aba111076bfe3fccbd0d6195b7b
Miguel Piedrafita <github@miguelpiedrafita.com>
2017-03-07 17:50:31 +0100

    Customize tag

--- src/OrgManagerDashboardServiceProvider.php
+++ src/OrgManagerDashboardServiceProvider.php
@@ -16,7 +16,7 @@ class OrgManagerDashboardServiceProvider extends ServiceProvider
         __DIR__.'/assets/js/app.js' => resource_path('assets/js/app.js'),
         __DIR__.'/assets/sass/components/_orgmanager-statistics.scss' => resource_path('assets/sass/components/_orgmanager-statistics.scss'),
         __DIR__.'/assets/sass/app.scss' => resource_path('assets/sass/app.scss'),
-    ], 'assets');
+    ], 'orgmanager-dashboard/assets');
     }
 
     /**
