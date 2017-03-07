984f28075312a291f75aaabbce1448df5f982457
Miguel Piedrafita <github@miguelpiedrafita.com>
2017-03-07 18:09:51 +0100

    Console check

--- src/OrgManagerDashboardServiceProvider.php
+++ src/OrgManagerDashboardServiceProvider.php
@@ -11,12 +11,14 @@ class OrgManagerDashboardServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->publishes([
-        __DIR__.'/assets/js/components/OrgManagerData.vue' => resource_path('assets/js/components/OrgManagerData.vue'),
-        __DIR__.'/assets/js/app.js' => resource_path('assets/js/app.js'),
-        __DIR__.'/assets/sass/components/_orgmanager-statistics.scss' => resource_path('assets/sass/components/_orgmanager-statistics.scss'),
-        __DIR__.'/assets/sass/app.scss' => resource_path('assets/sass/app.scss'),
-    ], 'orgmanager-dashboard/assets');
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/assets/js/components/OrgManagerData.vue' => resource_path('assets/js/components/OrgManagerData.vue'),
+                __DIR__.'/assets/js/app.js' => resource_path('assets/js/app.js'),
+                __DIR__.'/assets/sass/components/_orgmanager-statistics.scss' => resource_path('assets/sass/components/_orgmanager-statistics.scss'),
+                __DIR__.'/assets/sass/app.scss' => resource_path('assets/sass/app.scss'),
+            ], 'orgmanager-dashboard/assets');
+        }
     }
 
     /**
