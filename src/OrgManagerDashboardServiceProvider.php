d8012a1e2e57dd0ba2c3e1f6424923f5a6f6eded
Miguel Piedrafita <github@miguelpiedrafita.com>
2017-03-07 17:41:27 +0100

    Add Main assets

--- src/OrgManagerDashboardServiceProvider.php
+++ src/OrgManagerDashboardServiceProvider.php
@@ -13,7 +13,9 @@ class OrgManagerDashboardServiceProvider extends ServiceProvider
     {
         $this->publishes([
         __DIR__.'/assets/js/components/OrgManagerData.vue' => resource_path('assets/js/components/OrgManagerData.vue'),
+        __DIR__.'/assets/js/app.js' => resource_path('assets/js/app.js'),
         __DIR__.'/assets/sass/components/_orgmanager-statistics.scss' => resource_path('assets/sass/components/_orgmanager-statistics.scss'),
+        __DIR__.'/assets/sass/app.scss' => resource_path('assets/sass/app.scss'),
     ], 'assets');
     }
 
